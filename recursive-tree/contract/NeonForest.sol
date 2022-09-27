b54b2bd3c3ddc76e1520db3dd5cc523644bc7221
The Luke <luke@SPStories.com>
2022-09-27 14:12:44 +0700

    changed file structure

--- recursive-tree/contract/NeonForest.sol
+++ recursive-tree/contract/NeonForest.sol
@@ -6,19 +6,21 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, Ownable {
+contract NeonForest is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("Neon Forest", "NEON") {}
 
-    function safeMint(address to, string memory uri) {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
+
+    // The following functions are overrides required by Solidity.
 
     function _burn(uint256 tokenId)
         internal
